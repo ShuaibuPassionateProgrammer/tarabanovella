e8fec57ed234f9f58adb8c0892a4620cb368d163
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2023-11-20 16:40:54 +0100

    nav.php is created

---
+++ includes/nav.php
@@ -0,0 +1,12 @@
+<nav class="navbar navbar-light navbar-expand-lg fixed-top" id="mainNav">
+        <div class="container"><a class="navbar-brand" href="index.html">TARABA NOVELLA</a><button data-toggle="collapse" data-target="#navbarResponsive" class="navbar-toggler" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><i class="fa fa-bars"></i></button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item"><a class="nav-link" href="/tsunovella">Home</a></li>
+                    <li class="nav-item"><a class="nav-link" href="post.php">View Journals</a></li>
+                    <li class="nav-item"><a class="nav-link" href="contact.php">Contact us</a></li>
+                    <li class="nav-item"><a class="nav-link" href="about.php">About us</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
