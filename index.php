94b2b09720ecb4bb48325e4438ecf8943acec3e9
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2023-11-20 17:09:40 +0100

    Header and its content is added

--- index.php
+++ index.php
@@ -1,4 +1,17 @@
 <?php require("includes/header.php"); ?>
 <?php require("includes/nav.php"); ?>
 
+    <header class="masthead" style="background-image:url('assets/img/home-bg.jpg');">
+        <div class="overlay"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-10 col-lg-8 mx-auto">
+                    <div class="site-heading">
+                        <h1>TARABA NOVELLA</h1><span class="subheading">Journal of multidisciplinary studies</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
 <?php require("includes/footer.php"); ?>
