73bbb65dd095f30b008d2f6d2f78b29d3107a867
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2023-11-20 16:30:36 +0100

    dbconf.php is created

---
+++ includes/dbconf.php
@@ -0,0 +1,18 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'tsu_novella');
+
+try
+{
+    $dbcon = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME, DB_USER, DB_PASS);
+    $dbcon->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // echo "Database connected successfully!";
+}
+catch(PDOException $e)
+{
+    echo "Error: ".$e->getMessage();
+}
+?>
