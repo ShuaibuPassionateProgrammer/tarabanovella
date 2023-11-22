051f8791a5e97fed3dd5258b3e5c022d4404ca08
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2023-11-22 11:26:19 +0100

    Done with the structure to add the journal

--- admin/add_journals.php
+++ admin/add_journals.php
@@ -3,6 +3,44 @@
 <?php require('includes/sidebar.php'); ?>
 <?php require('includes/topbar.php'); ?>
 
+<div class="container m-4 p-4 pr-5" style="background-color: #fff;">
+    <h2 class="mb-4">Upload Journal</h2>
 
+    <form action="process.php" method="post" enctype="multipart/form-data">
+        <div class="form-group">
+            <label for="title">Title:</label>
+            <input type="text" class="form-control" name="title" required>
+        </div>
+
+        <div class="form-group">
+            <label for="author">Author:</label>
+            <input type="text" class="form-control" name="author" required>
+        </div>
+
+        <div class="form-group">
+            <label for="description">Description:</label>
+            <textarea class="form-control" name="description" style="height: 150px; resize: none;" required></textarea>
+        </div>
+
+        <div class="form-group">
+            <label for="publication_date">Publication Date and Time:</label>
+            <input type="datetime-local" class="form-control" name="publication_date" required>
+        </div>
+
+        <div class="form-group">
+            <label for="file">Upload Journal (PDF):</label>
+            <div class="input-group">
+                <div class="custom-file">
+                    <input type="file" class="custom-file-input border" id="customFile" name="file" accept=".pdf" required onchange="previewFile()">
+                    <label class="custom-file-label" for="customFile">Choose file</label>
+                </div>
+            </div>
+        </div>
+
+        <div id="preview"></div>
+
+        <button type="submit" class="btn btn-primary" name="upload_journals">Upload</button>
+    </form>
+</div>
 
 <?php require('includes/footer.php'); ?>
