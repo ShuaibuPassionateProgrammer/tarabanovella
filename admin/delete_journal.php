08f5594ef876c236a75ffe8e996730a27ebfa93f
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2023-11-22 11:41:17 +0100

    A mechanism for error handling

--- admin/delete_journal.php
+++ admin/delete_journal.php
@@ -3,6 +3,11 @@ session_start();
 
 if(isset($_GET['id'])) {
     require("includes/dbconf.php");
+
+    try {}
+    catch (PDOException $e) {
+        echo "Error: ".$e->getMessage();
+    }
 }
 else {
     header("location: journal_list.php");
