37eed431f6cf7f33213c293cc7dc8125153767c1
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2023-11-22 12:03:11 +0100

    Check if the search form is submitted

--- admin/search_journals.php
+++ admin/search_journals.php
@@ -5,3 +5,18 @@ require('includes/sidebar.php');
 require('includes/topbar.php');
 
 require('includes/dbconf.php');
+
+if (isset($_POST['search'])) {
+    $searchTerm = $_POST['searchTerm'];
+
+    // Fetch journals based on the search term in title or author
+    try {
+        $stmt = $dbcon->prepare("SELECT * FROM tbl_journals WHERE title LIKE :searchTerm OR author LIKE :searchTerm ORDER BY publication_date DESC");
+        $stmt->bindValue(':searchTerm', '%' . $searchTerm . '%', PDO::PARAM_STR);
+        $stmt->execute();
+        $searchResults = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    } catch (PDOException $e) {
+        echo "Error: " . $e->getMessage();
+    }
+}
+?>
