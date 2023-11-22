931f437d514d3b16ca55ccb3e4fe6a678400c5f5
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2023-11-22 07:30:31 +0100

    Database connection established

--- admin/includes/dconf.php
+++ admin/includes/dconf.php
@@ -4,7 +4,12 @@ define("DB_USER", "root");
 define("DB_PASS", "");
 define("DB_NAME", "taraba_novella");
 
-try {}
+try {
+    $dbcon = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME, DB_USER, DB_PASS);
+    $dbcon->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // echo "Database connected successfully!";
+}
 catch (PDOException $e) {
     echo "Error: " .$e->getMessage();
 }
