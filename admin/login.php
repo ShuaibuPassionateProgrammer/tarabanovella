1f4d3881052123aaffa7015d8d49ba08f9164c6d
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2023-11-22 10:45:02 +0100

    Setup all the necssary script for the Admin login

--- admin/login.php
+++ admin/login.php
@@ -35,5 +35,10 @@ if(isset($_SESSION['admin_login']) !== false)
 </head>
 <body>
     
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.9.2/umd/popper.min.js" integrity="sha512-2rNj2KJ+D8s1ceNasTIex6z4HWyOnEYLVC3FigGOmyQCZc2eBXKgOxQmo3oKLHyfcj53uz4QMsRCWNbLd32Q1g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdn.usebootstrap.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script src="assets/js/script.min.js"></script>
 </body>
 </html>
