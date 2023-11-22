c9788b736db13cd0f7cb7bdd3bb8caece5a95b7e
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2023-11-22 10:42:00 +0100

    Admin login boiler plate generated

--- admin/login.php
+++ admin/login.php
@@ -5,3 +5,16 @@ if(isset($_SESSION['admin_login']) !== false)
     header("location: /tarabanovella/admin");
     exit(0);
 }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Login</title>
+</head>
+<body>
+    
+</body>
+</html>
