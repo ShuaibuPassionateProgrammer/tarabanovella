abc49c69e904322c35032038ae6bcf8cf47cd7ee
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2023-11-22 11:58:55 +0100

    Fetch feedback messages from tbl_contact_messages

--- admin/feedback.php
+++ admin/feedback.php
@@ -5,5 +5,14 @@ require('includes/sidebar.php');
 require('includes/topbar.php');
 require('includes/dbconf.php');
 
+try {
+    $stmt = $dbcon->prepare("SELECT * FROM tbl_contact_messages");
+    $stmt->execute();
+    $messages = $stmt->fetchAll(PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+
+
 require('includes/footer.php');
 ?>
