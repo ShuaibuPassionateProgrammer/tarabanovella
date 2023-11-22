9eab2a059afd39b6f8f5b2448d27b61581cb23ad
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2023-11-22 11:49:58 +0100

    Done with delete journal logics

--- admin/delete_journal.php
+++ admin/delete_journal.php
@@ -2,13 +2,14 @@
 session_start();
 
 if(isset($_GET['id'])) {
-    require("includes/dbconf.php");
+    require('includes/dbconf.php');
 
     try {
         $journalId = $_GET['id'];
 
+        // Fetch the file path before deleting the record
         $stmt = $dbcon->prepare("SELECT file_path FROM tbl_journals WHERE id = :id");
-        $stmt->bindParam(":id", $journalId);
+        $stmt->bindParam(':id', $journalId);
         $stmt->execute();
         $result = $stmt->fetch(PDO::FETCH_ASSOC);
 
@@ -24,14 +25,14 @@ if(isset($_GET['id'])) {
         } else {
             $_SESSION['del_journal_status'] = "Error: Journal not found.";
         }
-    }
-    catch (PDOException $e) {
-        echo "Error: ".$e->getMessage();
+    } catch (PDOException $e) {
+        echo "Error: " . $e->getMessage();
     }
 
     header("location: journal_list.php");
     exit();
-}
-else {
+} else {
     header("location: journal_list.php");
+    exit();
 }
+?>
