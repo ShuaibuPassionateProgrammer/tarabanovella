b0201765cc59cdee75777f5e427c9f5bc96b3ad0
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2023-11-22 05:58:36 +0100

    A contact form is created inside the container

--- contact.php
+++ contact.php
@@ -1,6 +1,36 @@
 <?php require('includes/header.php'); ?>
 <?php require('includes/nav.php'); ?>
 
-
+<div class="container">
+    <div class="row">
+        <div class="col-md-10 col-lg-8 mx-auto">
+            <p>Want to get in touch? Fill out the form below to send us a message, and we will get back to you as soon as possible!</p>
+            <form id="contactForm" name="contactForm" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>" novalidate="novalidate">
+                <div class="form-group">
+                    <label for="name">Name</label>
+                    <input type="text" class="form-control" id="name" name="name" required placeholder="Enter your name">
+                    <div class="invalid-feedback">Please enter your name.</div>
+                </div>
+                <div class="form-group">
+                    <label for="email">Email Address</label>
+                    <input type="email" class="form-control" id="email" name="email" required placeholder="Enter your email">
+                    <div class="invalid-feedback">Please enter a valid email address.</div>
+                </div>
+                <div class="form-group">
+                    <label for="phone">Phone Number</label>
+                    <input type="tel" class="form-control" id="phone" name="phone" required placeholder="Enter your phone number">
+                    <div class="invalid-feedback">Please enter a valid phone number.</div>
+                </div>
+                <div class="form-group">
+                    <label for="message">Message</label>
+                    <textarea class="form-control" id="message" name="message" required placeholder="Enter your message" rows="5"></textarea>
+                    <div class="invalid-feedback">Please enter your message.</div>
+                </div>
+                <div id="success"></div>
+                <button class="btn btn-primary" id="sendMessageButton" name="sendMessageButton" type="submit">Send Message</button>
+            </form>
+        </div>
+    </div>
+</div>
 
 <?php require('includes/footer.php'); ?>
