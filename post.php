38153e73e90dd5532c5c418b62754e21cc7b5e92
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2023-11-22 06:14:52 +0100

    Header defined for the post.php file

--- post.php
+++ post.php
@@ -1,5 +1,17 @@
 <?php require('includes/header.php'); ?>
 <?php require('includes/nav.php'); ?>
 
+    <header class="masthead" style="background-image:url('assets/img/post-bg.jpg');">
+        <div class="overlay"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-10 col-lg-8 mx-auto">
+                    <div class="post-heading">
+                        <br>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
 
 <?php require('includes/footer.php'); ?>
