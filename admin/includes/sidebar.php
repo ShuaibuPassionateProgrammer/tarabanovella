9dc5851227b442dae8f753f9adde6324297a343c
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2023-11-22 07:37:46 +0100

    Done setting up the sidebar

---
+++ admin/includes/sidebar.php
@@ -0,0 +1,15 @@
+<div id="wrapper">
+        <nav class="navbar navbar-dark align-items-start sidebar sidebar-dark accordion bg-gradient-primary p-0">
+            <div class="container-fluid d-flex flex-column p-0"><a class="navbar-brand d-flex justify-content-center align-items-center sidebar-brand m-0" href="#">
+                    <!-- <div class="sidebar-brand-icon rounded-10"><img src="assets/img/avatars/tsu_logo.png" class="w-100" /></div> -->
+                    <div class="sidebar-brand-text mx-3"><span>TARABA NOVELLA</span></div>
+                </a>
+                <hr class="sidebar-divider my-0">
+                <ul class="navbar-nav text-light" id="accordionSidebar">
+                    <li class="nav-item"><a class="nav-link active" href="index.php"><i class="fas fa-tachometer-alt"></i><span>Dashboard</span></a></li>
+                    <li class="nav-item"><a class="nav-link" href="journal.php"><i class="fas fa-journal-whills"></i><span>Journals</span></a></li>
+                    <li class="nav-item"><a class="nav-link" href="feedback.php"><i class="far fa-comments"></i><span>Feedback</span></a></li>
+                </ul>
+                <div class="text-center d-none d-md-inline"><button class="btn rounded-circle border-0" id="sidebarToggle" type="button"></button></div>
+            </div>
+        </nav>
