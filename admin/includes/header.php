c708a76a2935f763baf4a3f38fa1d004f1eff82d
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2023-11-22 07:35:24 +0100

    Done with refactoring the header

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -6,48 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>TARABA NOVELLA</title>
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.0/css/all.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <style>
-        .container {
-            padding-top: 20px;
-            padding-bottom: 20px;
-        }
-
-        .journal-card {
-            margin-bottom: 20px;
-        }
-
-        .journal-image {
-            width: 100%;
-            height: auto;
-        }
-
-        .journal-title {
-            font-size: 24px;
-            font-weight: bold;
-            margin-top: 10px;
-            margin-bottom: 10px;
-        }
-
-        .journal-excerpt {
-            font-size: 18px;
-            margin-bottom: 10px;
-        }
-
-        .journal-date {
-            font-size: 14px;
-            color: #6c757d;
-            margin-bottom: 5px;
-        }
-
-        .journal-readmore {
-            font-size: 16px;
-            color: #007bff;
-            text-decoration: none;
-        }
-    </style>
+    <link rel="stylesheet" href="assets/fonts/fontawesome5-overrides.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 
-<body>
+<body id="page-top">
