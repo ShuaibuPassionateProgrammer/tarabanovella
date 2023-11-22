5bb1fd39e70102e106779fd7fff1c9880b66d180
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2023-11-22 11:45:58 +0100

    Fetch the file path before deleting the record

--- admin/delete_journal.php
+++ admin/delete_journal.php
@@ -6,6 +6,11 @@ if(isset($_GET['id'])) {
 
     try {
         $journalId = $_GET['id'];
+
+        $stmt = $dbcon->prepare("SELECT file_path FROM tbl_journals WHERE id = :id");
+        $stmt->bindParam(":id", $journalId);
+        $stmt->execute();
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
     }
     catch (PDOException $e) {
         echo "Error: ".$e->getMessage();
