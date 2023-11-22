8a39ddde86d8e02a14bdca61aac462cc3fe2af46
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2023-11-22 11:08:26 +0100

    The main container is added to the journal

--- admin/journal.php
+++ admin/journal.php
@@ -3,4 +3,12 @@
 <?php require('includes/sidebar.php'); ?>
 <?php require('includes/topbar.php'); ?>
 
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <h2>Manage Journals</h2>
+        </div>
+    </div>
+</div>
+
 <?php require('includes/footer.php'); ?>
