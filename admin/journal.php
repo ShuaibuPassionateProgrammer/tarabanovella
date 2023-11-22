dbda58c5611f95089689acfab9d7e0e94b5998c1
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2023-11-22 11:09:05 +0100

    The new container is added to the journal

--- admin/journal.php
+++ admin/journal.php
@@ -11,4 +11,38 @@
     </div>
 </div>
 
+<div class="container mt-4">
+    <div class="row">
+        <div class="col-lg-4 mb-4">
+            <div class="card h-100">
+                <div class="card-body">
+                    <h5 class="card-title">Add New Journal</h5>
+                    <p class="card-text">Add a new journal entry to the system.</p>
+                    <a href="add_journals.php" class="btn btn-primary">Add Journal</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-lg-4 mb-4">
+            <div class="card h-100">
+                <div class="card-body">
+                    <h5 class="card-title">View Journal List</h5>
+                    <p class="card-text">View the details of a journal, including the author and content.</p>
+                    <a href="journal_list.php" class="btn btn-primary">View Journal</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-lg-4 mb-4">
+            <div class="card h-100">
+                <div class="card-body">
+                    <h5 class="card-title">Search Journal</h5>
+                    <p class="card-text">Search for journals by title, author, or publication date.</p>
+                    <a href="search_journals.php" class="btn btn-primary">Search Journals</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 <?php require('includes/footer.php'); ?>
