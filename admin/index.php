02f56c8f26d32d047bdb44e0c8ccc7db73d7ec37
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2023-11-22 10:52:57 +0100

    Done setting up the necessary requiremen for the Admin Dashboard/home

--- admin/index.php
+++ admin/index.php
@@ -3,4 +3,29 @@
 <?php require('includes/sidebar.php'); ?>
 <?php require('includes/topbar.php'); ?>
 
+                <div class="container-fluid">
+                    <!-- <div class="d-sm-flex justify-content-between align-items-center mb-4">
+                        <h3 class="text-dark mb-0">Dashboard</h3><a class="btn btn-primary btn-sm d-none d-sm-inline-block" role="button" href="#"><i class="fas fa-download fa-sm text-white-50"></i>&nbsp;Explore More</a>
+                    </div> -->
+                    
+                    <div class="row">
+                        <div class="col-lg-6 mb-4">
+                            <div class="card shadow mb-4">
+                                <div class="card-header py-3">
+                                    <h6 class="text-primary font-weight-bold m-0">Welcome, Admin</h6>
+                                </div>
+                                
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="row justify-content-center m-2">
+                        <div class="col-md-12">
+                            <img src="assets/img/avatars/largepreview.png" width="100%" height="500px;">                
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+
 <?php require('includes/footer.php'); ?>
