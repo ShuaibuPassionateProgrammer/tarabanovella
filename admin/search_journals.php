a8c26dd38cc45cd28f8653bbf8c36a10b76917a6
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2023-11-22 12:04:11 +0100

    Done with all the necessary logic inside the search_journal

--- admin/search_journals.php
+++ admin/search_journals.php
@@ -20,3 +20,50 @@ if (isset($_POST['search'])) {
     }
 }
 ?>
+
+<div class="container m-4 p-4 pr-5" style="background-color: #fff;">
+    <h2 class="mb-4">Search Journals</h2>
+
+    <!-- Search Form -->
+    <form action="search_journals.php" method="post">
+        <div class="form-group">
+            <label for="searchTerm">Search by Title or Author:</label>
+            <input type="text" class="form-control form-control-lg" name="searchTerm" placeholder="Enter journal title or author" required>
+        </div>
+        <button type="submit" class="btn btn-primary" name="search">Search</button>
+    </form>
+
+    <?php if (isset($searchResults) && !empty($searchResults)): ?>
+        <!-- Display search results in a table -->
+        <h3 class="mt-4">Search Results</h3>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Title</th>
+                    <th>Author</th>
+                    <th>Description</th>
+                    <th>Publication Date</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($searchResults as $result): ?>
+                    <tr>
+                        <td><?php echo $result['title']; ?></td>
+                        <td><?php echo $result['author']; ?></td>
+                        <td><?php echo $result['description']; ?></td>
+                        <td><?php echo $result['publication_date']; ?></td>
+                        <td>
+                            <a href="<?php echo $result['file_path']; ?>" target="_blank">Download</a>
+                            <a href="delete_journal.php?id=<?php echo $result['id']; ?>" class="btn btn-danger btn-sm" onclick="return confirm('Are you sure you want to delete this journal?')">
+                                <i class="fa fa-trash"></i> <!-- Font Awesome delete icon -->
+                            </a>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    <?php elseif (isset($searchResults) && empty($searchResults)): ?>
+        <p>No matching journals found.</p>
+    <?php endif; ?>
+</div>
