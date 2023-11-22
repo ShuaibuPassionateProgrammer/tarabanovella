63082b2122b2f720a15fd2dd6677a716db5d0fe9
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2023-11-22 05:54:30 +0100

    The main content of the about implemented inside container

--- about.php
+++ about.php
@@ -15,4 +15,69 @@
     </div>
 </header>
 
+<div class="container">
+    <h1 class="text-center mb-4">ABOUT TARABA NOVELLA</h1>
+
+    <div class="about-content">
+        <h1>EDITORIAL POLICIES</h1>
+        <h2>Overview </h2>
+        <p>
+            This policy specifies guidelines for our publications' process. In particular, Academic publications embrace and endeavour to comply to the following standards and requirements:
+        </p>
+
+        <h2>Authorship</h2>
+        <p>
+            An author is defined as someone who has significantly contributed to the development of a manuscript journal. It is advised that authorship be based on the three criteria listed below. 
+            Significant contributions to the work's idea or design; or the collecting, analysis, or interpretation of data for the work; And draft the work or critically revise it for key intellectual content; AND final approval of the published version; AND Acceptance of responsibility for all aspects of the work, including ensuring that any questions about the accuracy or integrity of any portion of the work are appropriately investigated and handled.
+        </p>
+
+        <h2>Acknowledgment</h2>
+        <p>
+            Individuals who contributed to the production of a Manuscript but do not qualify as authors should be thanked. Organisations that contributed funding and/or other resources should also be mentioned.
+        </p>
+
+        <h2>Changes in Authorship</h2>
+        <p>
+            Whenever changes to the authorship of a Manuscript or a published article are required, the changes will be executed in accordance with the specifications. Only correspondent writers can request a change of authorship. Requests should be addressed to the editor using the modifications in the Authorship Form.
+        </p>
+
+        <h2>Submission of Manuscript</h2>
+        <p>
+            Before submitting a paper, authors should read the ''Instructions for Authors" on the journal's website. Manuscripts should be produced in accordance with the journal's style and specifications.
+            The authors named on the manuscript must have met the authorship standards outlined above. Whenever possible, specify each author's contribution.
+            Prior to submission, all Authors should approve the final version of the work. Once a paper is submitted, it is presumed that all authors have reviewed it and given their approval for its submission.
+            All authors' contact information, including surname/other names, affiliation, e-mail, and phone/fax number, should be included in the paper.
+            Conflicts of interest should be declared in the paper.
+            The submission must be made online at: <a href="mailto:tsunovellajournal@gmail.com">tsunovellajournal@gmail.com</a>
+            <a href="mailto:tsunovellajournal@hotmail.com">tsunovellajournal@hotmail.com</a>
+            <a href="mailto:tsunovellajournal@yahoo.com">tsunovellajournal@yahoo.com</a>
+        </p>
+    </div>
+
+    <!-- <div class="about-content">
+        <p>
+            Welcome to TSU-Novella, your online journal platform where you can discover and share a wealth of knowledge,
+            insights, and stories. Whether you are a reader or an author, TSU-Novella provides a seamless experience
+            for exploring diverse journals in various fields.
+        </p>
+
+        <p>
+            Our mission is to create a space where individuals can express their thoughts, research findings,
+            and creative works. TSU-Novella aims to foster a community of writers, researchers, and readers,
+            promoting the exchange of ideas and perspectives.
+        </p>
+
+        <p>
+            Key features of TSU-Novella include user-friendly journal uploads, easy navigation, and a robust search
+            functionality to help you find journals that match your interests. Dive into the world of knowledge and
+            storytelling with TSU-Novella.
+        </p>
+
+        <p>
+            Thank you for being a part of our community. If you have any questions or feedback, feel free to
+            <a href="contact.php">contact us</a>.
+        </p>
+    </div> -->
+</div>
+
 <?php require('includes/footer.php'); ?>
