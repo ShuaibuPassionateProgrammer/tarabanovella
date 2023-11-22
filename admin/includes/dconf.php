7f8972556e8e851175a2b037a603e4e0d3f10aa9
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2023-11-22 07:25:56 +0100

    An error handling mechanism is added to the database connection file

--- admin/includes/dconf.php
+++ admin/includes/dconf.php
@@ -3,3 +3,8 @@ define("DB_HOST", "localhost");
 define("DB_USER", "root");
 define("DB_PASS", "");
 define("DB_NAME", "taraba_novella");
+
+try {}
+catch (PDOException $e) {
+    echo "Error: " .$e->getMessage();
+}
