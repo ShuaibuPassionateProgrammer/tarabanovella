5f229121fa9cc8aeb701342438c25230d5a8bfa8
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2023-11-22 05:53:14 +0100

    header about implemented

--- about.php
+++ about.php
@@ -1,4 +1,18 @@
 <?php require('includes/header.php'); ?>
 <?php require('includes/nav.php'); ?>
 
+<header class="masthead" style="background-image: url('assets/img/about-bg.jpg');">
+    <div class="overlay"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-lg-8 mx-auto">
+                <div class="site-heading">
+                    <h1>About Us</h1>
+                    <span class="subheading">This is what we do</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+
 <?php require('includes/footer.php'); ?>
